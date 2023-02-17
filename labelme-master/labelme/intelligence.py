2e3ef1cdc7de00f01f7ee3b823dbbf899641d9b1
yousefelsharkawy <yousefelsharkawy111@gmail.com>
2023-02-17 17:09:46 +0200

    solving a bug for cpu usage of the application

--- labelme-master/labelme/intelligence.py
+++ labelme-master/labelme/intelligence.py
@@ -77,7 +77,7 @@ class Intelligence():
         # model = init_detector("C:/Users/Shehab/Desktop/l001/ANNOTATION_TOOL/mmdetection/mmdetection/configs/detectors/htc_r50_sac_1x_coco.py", 
         #                     "C:/Users/Shehab/Desktop/l001/ANNOTATION_TOOL/mmdetection/mmdetection/checkpoints/htc_r50_sac_1x_coco-bfa60c54.pth", device = torch.device("cuda"))
         model = init_detector(config, 
-                            checkpoint, device = torch.device("cuda"))
+                            checkpoint, device = torch.device("cuda" if torch.cuda.is_available() else "cpu"))
         # "C:\Users\Shehab\Desktop\l001\ANNOTATION_TOOL\mmdetection\mmdetection\configs\yolact\yolact_r50_1x8_coco.py"
         # model = init_detector("C:/Users/Shehab/Desktop/mmdetection/mmdetection/configs/detectors/htc_r50_sac_1x_coco.py",
                             # "C:/Users/Shehab/Desktop/mmdetection/mmdetection/checkpoints/htc_r50_sac_1x_coco-bfa60c54.pth", device = torch.device("cuda"))
