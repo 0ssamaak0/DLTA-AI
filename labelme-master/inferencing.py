3bfac408ca51bb7312b2d5ecb8dda67d2b5cf692
Usama Ahmed <0ssamaak0@gmail.com>
2023-03-08 10:10:57 +0300

    testing the branch

--- labelme-master/inferencing.py
+++ labelme-master/inferencing.py
@@ -1,4 +1,5 @@
 
+from time import time
 import sys
 import torch
 from mmdet.apis import inference_detector, init_detector , async_inference_detector
@@ -9,11 +10,10 @@ from scipy.interpolate import splprep, splev
 import json
 import warnings
 warnings.filterwarnings("ignore")
-from time import time
 
 
 class models_inference():
-    
+
     def interpolate_polygon(self , polygon, n_points):
         # interpolate polygon to get less points
         polygon = np.array(polygon)
@@ -29,7 +29,8 @@ class models_inference():
     # function to masks into polygons
     def mask_to_polygons(self , mask, n_points=20):
         # Find contours
-        contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+        contours = cv2.findContours(
+            mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
         # Convert contours to polygons
         polygon = []
         for contour in contours:
@@ -38,15 +39,15 @@ class models_inference():
             if contour[-2:] == contour[:2]:
                 contour = contour[:-2]
             polygon.append(contour)
-        polygon = [(polygon[0][i], polygon[0][i + 1]) for i in np.arange(0, len(polygon[0]), 2)]
+        polygon = [(polygon[0][i], polygon[0][i + 1])
+                   for i in np.arange(0, len(polygon[0]), 2)]
         polygon = self.interpolate_polygon(polygon, n_points)
         return polygon
 
+    # experimental
 
-
-
-    def decode_file(self, img , model, classdict,threshold=0.3,img_array_flag=False):
-    # def decode_file(self, img_path, threshold=0.3 , selected_model_name=""):
+    def decode_file(self, img , model, classdict, threshold=0.3, img_array_flag=False):
+        # def decode_file(self, img_path, threshold=0.3 , selected_model_name=""):
         # if img_path is none img_path = "test_img_1.webp"
 
         # with open("saved_models.json") as json_file:
@@ -61,43 +62,39 @@ class models_inference():
         #         checkpoint = data[selected_model_name]["checkpoint"]
         #     # print(f'selected model : {selected_model_name} \n config : {config} \n\
         #     #     checkpoint : {checkpoint} \n')
-        print(classdict)   
-        
+        print(classdict)
+
         # torch.cuda.empty_cache()
-        
+
         # model = init_detector(config, checkpoint, device = torch.device("cuda"))
         if img_array_flag:
             results = inference_detector(model, img)
         else:
             results = inference_detector(model, plt.imread(img))
         # results = async_inference_detector(model, plt.imread(img_path))
-        
 
         # del model
         torch.cuda.empty_cache()
 
-        
         # print(f'ram used : {torch.cuda.memory_allocated() / 1024 ** 3} GB')
         # print(f'ram cached : {torch.cuda.memory_cached() / 1024 ** 3} GB')
         # print(f'what is using the ram : {torch.cuda.memory_summary()}')
 
-        
-
         #results0 = [results[0][0], results[0][2],results[0][3],results[0][5],results[0][7]]
         #results1 = [results[1][0], results[1][2],results[1][3],results[1][5],results[1][7]]
-        
+
         results0 = []
         results1 = []
         for i in classdict.keys():
             results0.append(results[0][i])
             results1.append(results[1][i])
-        
+
         result_dict = {}
         res_list = []
-        
+
         def full_points(bbox):
             return np.array([[bbox[0], bbox[1]], [bbox[0], bbox[3]], [bbox[2], bbox[3]], [bbox[2], bbox[1]]])
-        
+
         #classdict = {0:"person", 1:"car", 2:"motorcycle", 3:"bus", 4:"truck"}
         classes_numbering = [keyno for keyno in classdict.keys()]
         for classno in range(len(results0)):
@@ -108,11 +105,14 @@ class models_inference():
                 result["class"] = classdict.get(classes_numbering[classno])
                 # Confidence
                 result["confidence"] = str(results0[classno][instance][-1])
-                result["bbox"] = results0[classno][instance][:-1].astype(np.uint8)
+                result["bbox"] = results0[classno][instance][:-
+                                                             1].astype(np.uint8)
                 if classno == 0:
-                    result["seg"] = self.mask_to_polygons(results1[classno][instance].astype(np.uint8) , 10)
-                else : 
-                    result["seg"] = self.mask_to_polygons(results1[classno][instance].astype(np.uint8))
+                    result["seg"] = self.mask_to_polygons(
+                        results1[classno][instance].astype(np.uint8) , 10)
+                else :
+                    result["seg"] = self.mask_to_polygons(
+                        results1[classno][instance].astype(np.uint8))
                 # result["bbox"] = full_points(result["bbox"]).tolist()
                 # points = full_points(result["bbox"])
                 # result["x1"] = points[0][0]
