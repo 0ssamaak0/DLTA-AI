51f42b37258cacb39ac79ae1deb8d8921b49ad96
shehab <shehabthelegand@gmail.com>
2023-03-21 10:45:51 +0200

    update byte tracker args

--- labelme-master/labelme/app.py
+++ labelme-master/labelme/app.py
@@ -60,9 +60,11 @@ warnings.filterwarnings("ignore")
 
 @dataclass(frozen=True)
 class BYTETrackerArgs:
-    track_thresh: float = 0.25
-    track_buffer: int = 30
-    match_thresh: float = 0.8
+    track_thresh: float = 0.6  # tracking confidence threshold
+    track_buffer: int = 30   # the frames for keep lost tracks
+    match_thresh: float = 0.8  # matching threshold for tracking
+    conf_thres: float = 0.5122620708221085
+    frame_rate: int = 30     # FPS
     aspect_ratio_thresh: float = 3.0
     min_box_area: float = 1.0
     mot20: bool = False
