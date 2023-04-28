867cc7ff372ddbeaf5c4064b7a2a26877d806b9b
yousefelsharkawy <yousefelsharkawy111@gmail.com>
2023-04-28 03:21:55 +0300

    adding a configuration json for sam models

---
+++ labelme-master/models_menu/samScraper.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import requests
+
+url = 'https://github.com/facebookresearch/segment-anything/blob/main/README.md'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# get all ul inside article tag
+ul = soup.find('article').find_all('ul')
+
+models_json = []
+# get all li inside ul
+li = ul[0].find_all('li')
+for i in li:
+    model = {}
+    #print(i.find('a').text.split(" ")[0]) # get text inside a tag (model name)
+    model['name'] = i.find('a').text.split(" ")[0]
+    #print(i.find('a')['href']) # get href inside a tag)
+    model['url'] = i.find('a')['href']
+    model['checkpoint'] = i.find('a')['href'].split("/")[-1]
+    models_json.append(model)
+
+with open ("sam_models.json", "w") as f:
+            json.dump(models_json, f, indent=4)
