f21b3d8a6bba2a0b67330d5b00a0dbf56b479ff1
Usama Ahmed <0ssamaak0@gmail.com>
2023-05-20 18:05:15 +0300

    Context Menu and Edit menu organization Bug fixes in tracking

--- labelme-master/labelme/utils/helpers.py
+++ labelme-master/labelme/utils/helpers.py
@@ -1873,6 +1873,36 @@ def getIDfromUser_GUI(self, group_id, text):
     return group_id, text
 
 
+def notification(text):
+    """
+    Sends a desktop notification with the given text.
+
+    Args:
+        text (str): The text to display in the notification.
+
+    Returns:
+        None
+    """
+    try:
+        from notifypy import Notify
+        # Create a Notify object with the default title
+        notification = Notify(default_notification_title="DLTA-AI")
+
+        # Set the message of the notification to the given text
+        notification.message = text
+
+        # Set the notification icon
+        print(os.getcwd())
+        notification.icon = "labelme/icons/icon.ico"
+
+        # Send the notification asynchronously
+        notification.send(block=False)
+    except Exception as e:
+        print(e)
+        print("please install notifypy to get desktop notifications")
+
+
+
 
 
 
