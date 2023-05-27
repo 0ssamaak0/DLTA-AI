ba00240c6dd4a7a599d3b4a16e38a4fc7c16e759
Usama Ahmed <0ssamaak0@gmail.com>
2023-05-27 23:10:14 +0300

    function documentation new comments added

--- DLTA-AI-app/labelme/utils/help.py
+++ DLTA-AI-app/labelme/utils/help.py
@@ -1,18 +1,38 @@
 from .helpers import OKmsgBox
 
-
 def show_runtime_data():
+    """
+    Function Name: show_runtime_data()
+
+    Description:
+    This function displays a dialog box with information about the runtime data of the system, including GPU and RAM stats.
+
+    Parameters:
+    This function takes no parameters.
+
+    Returns:
+    This function does not return anything.
+
+    Libraries:
+    This function requires the following libraries to be installed:
+    - PyQt5
+    - psutil
+    - torch
+    """
+    # Import necessary modules
     from PyQt5.QtWidgets import QDialog, QLabel, QVBoxLayout
     from PyQt5.QtGui import QFont
     import psutil
     import torch
 
+    # Create a dialog box to display the runtime data
     dialog = QDialog()
     dialog.setWindowTitle("DLTA-AI Runtime Data")
     layout = QVBoxLayout(dialog)
     layout.setContentsMargins(20, 20, 20, 20)
     layout.setSpacing(10)
 
+    # Set font styles for the title and normal text
     title_font = QFont()
     title_font.setPointSize(12)
     title_font.setBold(True)
@@ -20,7 +40,7 @@ def show_runtime_data():
     normal_font = QFont()
     normal_font.setPointSize(10)
 
-    # if cuda is available, print cuda name
+    # If CUDA is available, display GPU stats
     if torch.cuda.is_available():
         device_name = torch.cuda.get_device_name(0)
         gpu_title_label = QLabel("Device Stats")
@@ -37,11 +57,13 @@ def show_runtime_data():
         gpu_vram_label.setFont(normal_font)
         layout.addWidget(gpu_vram_label)
 
+    # If CUDA is not available, display CPU stats
     else:
         cpu_label = QLabel("DLTA-AI is Using CPU")
         cpu_label.setFont(title_font)
         layout.addWidget(cpu_label)
 
+    # Display RAM stats
     ram_title_label = QLabel("RAM Stats")
     ram_title_label.setFont(title_font)
     layout.addWidget(ram_title_label)
@@ -52,9 +74,29 @@ def show_runtime_data():
     ram_label.setFont(normal_font)
     layout.addWidget(ram_label)
 
+    # Display the dialog box
     dialog.exec_()
              
 def preferences():
+    """
+    Function Name: preferences()
+
+    Description:
+    This function displays a dialog box with preferences for the LabelMe application, including theme and notification settings.
+
+    Parameters:
+    This function takes no parameters.
+
+    Returns:
+    If the user clicks the OK button, this function writes the new theme and notification settings to the config file and returns `QtWidgets.QDialog.Accepted`. If the user clicks the Cancel button, this function does not write any changes to the config file and returns `QtWidgets.QDialog.Rejected`.
+
+    Libraries:
+    This function requires the following libraries to be installed:
+    - yaml
+    - PyQt5.QtWidgets
+    - PyQt5.QtGui
+    - PyQt5.QtCore
+    """
     import yaml
     from PyQt5 import QtWidgets, QtGui, QtCore
 
@@ -164,26 +206,44 @@ def preferences():
             yaml.dump(config, f)
 
     
-import yaml
-from PyQt5 import QtWidgets, QtGui, QtCore
 
 def shortcut_selector():
+    """
+    Displays a dialog box for selecting and editing keyboard shortcuts for the application.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # Import necessary modules
+    import yaml
+    from PyQt5 import QtWidgets, QtGui, QtCore
+
+    # Load the default shortcuts from the config file
     shortcuts = {}
     with open("labelme/config/default_config.yaml", "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
         shortcuts = config.get("shortcuts", {})
+
+    # Encode the shortcut names for display in the table
     shortcuts_names_encode = {name: name.lower().capitalize().replace("_", " ").replace("Sam", "SAM").replace("sam", "SAM") for name in shortcuts.keys()}
+
+    # Decode the shortcut names back to their original form
     shortcuts_names_decode = {value: key for key, value in shortcuts_names_encode.items()}
 
-    # change shortcuts keys using shortcut_names_encode
+    # Change the keys of the shortcuts dictionary to use the encoded names
     shortcuts = {shortcuts_names_encode[key]: value for key, value in shortcuts.items()}
 
+    # Create a table to display the shortcuts
     shortcut_table = QtWidgets.QTableWidget()
     shortcut_table.setColumnCount(2)
     shortcut_table.setHorizontalHeaderLabels(['Name', 'Shortcut'])
     shortcut_table.setRowCount(len(shortcuts))
     shortcut_table.verticalHeader().setVisible(False)
 
+    # Populate the table with the shortcut names and keys
     row = 0
     for name, key in shortcuts.items():
         name_item = QtWidgets.QTableWidgetItem(name)
@@ -192,6 +252,7 @@ def shortcut_selector():
         shortcut_table.setItem(row, 1, shortcut_item)
         row += 1
 
+    # Define a function to handle clicks on the shortcut table
     def on_shortcut_table_clicked(item):
         row = item.row()
         name_item = shortcut_table.item(row, 0)
@@ -211,6 +272,8 @@ def shortcut_selector():
         layout.addWidget(ok_button)
         layout.addWidget(null_hint_label)
         dialog.setLayout(layout)
+
+        # If the user clicks OK, update the shortcut and table
         if dialog.exec_():
             key = key_edit.keySequence().toString(QtGui.QKeySequence.NativeText)
             if key in shortcuts.values() and list(shortcuts.keys())[list(shortcuts.values()).index(key)] != name:
@@ -222,8 +285,10 @@ def shortcut_selector():
                 shortcuts[name] = key
                 shortcut_table.item(row, 1).setText(key)
 
+    # Connect the on_shortcut_table_clicked function to the itemClicked signal of the shortcut table
     shortcut_table.itemClicked.connect(on_shortcut_table_clicked)
 
+    # Create a dialog box to display the shortcut table
     dialog = QtWidgets.QDialog()
     dialog.setWindowTitle("Shortcuts")
     layout = QtWidgets.QVBoxLayout()
@@ -234,15 +299,21 @@ def shortcut_selector():
     note_label = QtWidgets.QLabel("Shortcuts will be updated after restarting the app.")
     layout.addWidget(note_label)
     dialog.setLayout(layout)
+
+    # Set the size of the dialog box
     dialog.setFixedWidth(shortcut_table.sizeHintForColumn(0) + shortcut_table.sizeHintForColumn(1) + 50)
     dialog.setMinimumHeight(shortcut_table.rowHeight(0) * 10 + 50)
-    
-    
+
     # Set the size policy to allow vertical resizing
     dialog.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Expanding)
-    
+
+    # Display the dialog box
     dialog.exec_()
+
+    # Decode the shortcut names back to their original form
     shortcuts = {shortcuts_names_decode[key]: value for key, value in shortcuts.items()}
+
+    # Write the updated shortcuts to the config file
     with open("labelme/config/default_config.yaml", "w") as f:
         config["shortcuts"] = shortcuts
         yaml.dump(config, f)
@@ -250,40 +321,96 @@ def shortcut_selector():
 
 
 def git_hub_link():
+    """
+    Opens the GitHub repository for the DLTA-AI project in the default web browser.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # Import necessary modules
     import webbrowser
+
+    # Open the GitHub repository in the default web browser
     webbrowser.open('https://github.com/0ssamaak0/DLTA-AI')  
-    return
 
 def open_issue():
+    """
+    Opens the GitHub issues page for the DLTA-AI project in the default web browser.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # Import necessary modules
     import webbrowser
+
+    # Open the GitHub issues page in the default web browser
     webbrowser.open('https://github.com/0ssamaak0/DLTA-AI/issues')
-    return
-                    
+
 def open_license():
+    """
+    Opens the license file for the DLTA-AI project in the default web browser.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # Import necessary modules
     import webbrowser
+
+    # Open the license file in the default web browser
     webbrowser.open('https://github.com/0ssamaak0/DLTA-AI/blob/master/LICENSE')
-    return
 
 def feedback():
+    """
+    Displays a dialog box for providing feedback on the DLTA-AI project.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # Define the text for the feedback dialog box
     text = "Found a bug? 🐞\nWant to suggest a feature? 🌟\n"
 
+    # Import necessary modules
     from PyQt5.QtWidgets import QMessageBox
     from PyQt5.QtCore import Qt
 
+    # Create the feedback dialog box
     msgBox = QMessageBox()
     msgBox.setWindowTitle("Feedback")
     msgBox.setText(text)
 
-    # add button when clicking on it will open github issues page
+    # Add a button to open the GitHub issues page
     msgBox.addButton(QMessageBox.Yes)
     msgBox.button(QMessageBox.Yes).setText("Open an Issue")
     msgBox.button(QMessageBox.Yes).clicked.connect(open_issue)
 
-    # add close button
+    # Add a close button
     msgBox.addButton(QMessageBox.Close)
     msgBox.button(QMessageBox.Close).setText("Close")
 
+    # Display the feedback dialog box
     msgBox.exec_()
 
 def version():
+    """
+    Displays the version number of the DLTA-AI project.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # Display the version number in a message box
     OKmsgBox("Version", "DLTA-AI Version 1.0")
